9f51a064865d829dcad9e37d30d55450add73199
Yves-Developer <yvesmugisha09@gmail.com>
2026-04-12 21:15:47 +0200

    ste bnsm,

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
+        {/* TODO: Add theme provider */}
       </body>
     </html>
   );
